02e16c1977198521018d897a1c6070988c9fcc76
Leonardo Jacques da Silva <leosul@gmail.com>
2023-09-21 14:46:01 +0100

    fix: change name solutions and install new package

--- ResultWrapperPackage/Controllers/WeatherForecastController.cs
+++ ResultWrapperPackage/Controllers/WeatherForecastController.cs
@@ -1,49 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
 using ResultWrapperPackage.Services;
 
-namespace ResultWrapperPackage.Controllers
+namespace ResultWrapperPackage.Controllers;
+
+[ApiController]
+[Route("api/v1/weather-forecasts")]
+public class WeatherForecastController : ControllerBase
 {
-    [ApiController]
-    [Route("api/v1/weather-forecasts")]
-    public class WeatherForecastController : ControllerBase
+    private readonly IWeatherForecastService _weatherForecastService;
+
+    public WeatherForecastController(IWeatherForecastService weatherForecastService)
     {
-        private readonly IWeatherForecastService _weatherForecastService;
+        _weatherForecastService = weatherForecastService;
+    }
 
-        public WeatherForecastController(IWeatherForecastService weatherForecastService)
-        {
-            _weatherForecastService = weatherForecastService;
-        }
+    [HttpGet("success/generics")]
+    public ActionResult GetGenericsSuccess()
+    {
+        var infos = _weatherForecastService.GetWeatherForecastsGenericWithSuccess();
 
-        [HttpGet("success/generics")]
-        public ActionResult GetGenericsSuccess()
-        {
-            var infos = _weatherForecastService.GetWeatherForecastsGenericWithSuccess();
+        return Ok(infos);
+    }
 
-            return Ok(infos);
-        }
+    [HttpGet("success/non-generics")]
+    public ActionResult GetNonGenericsSuccess()
+    {
+        var infos = _weatherForecastService.GetWeatherForecastsNonGenericWithSuccess();
 
-        [HttpGet("success/non-generics")]
-        public ActionResult GetNonGenericsSuccess()
-        {
-            var infos = _weatherForecastService.GetWeatherForecastsNonGenericWithSuccess();
+        return Ok(infos);
+    }
 
-            return Ok(infos);
-        }
+    [HttpGet("errors/generics")]
+    public ActionResult GetGenericErrors()
+    {
+        var infos = _weatherForecastService.GetWeatherForecastsGenericWithError();
 
-        [HttpGet("errors/generics")]
-        public ActionResult GetGenericErrors()
-        {
-            var infos = _weatherForecastService.GetWeatherForecastsGenericWithError();
+        return BadRequest(infos);
+    }
 
-            return BadRequest(infos);
-        }
+    [HttpGet("errors/non-generics")]
+    public ActionResult GetNonGenericErrors()
+    {
+        var infos = _weatherForecastService.GetWeatherForecastsNonGenericWithError();
 
-        [HttpGet("errors/non-generics")]
-        public ActionResult GetNonGenericErrors()
-        {
-            var infos = _weatherForecastService.GetWeatherForecastsNonGenericWithError();
-
-            return BadRequest(infos);
-        }
+        return BadRequest(infos);
     }
 }
